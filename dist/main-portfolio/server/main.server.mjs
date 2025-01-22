e307317f646fb5cef65ec34252a6f8bf76f8c044
Aness Rahmani <d.rahmanianess@gmail.com>
2025-01-22 14:21:04 +0100

    updating my instagram link and adding react to skills

--- dist/main-portfolio/server/main.server.mjs
+++ dist/main-portfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ACYJWJLX.mjs";import"./chunk-UVGVMMLZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-OBBDAMUG.mjs";import"./chunk-UVGVMMLZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
